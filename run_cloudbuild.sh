7952aa4084f7f54c25561c612994dd3d8735ce0e
weimeilin79 <weimeilin@gmail.com>
2025-08-12 08:26:11 -0400

    mini boss

--- run_cloudbuild.sh
+++ run_cloudbuild.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
-nohup gcloud builds submit --config cloudbuild.yaml "$@" > cloudbuild.log 2>&1 &
+nohup gcloud builds submit --config cloudbuild.yaml \
+  --substitutions=_REGION="$REGION",_REPO_NAME="$REPO_NAME",_SERVICE_NAME="agentverse-dungoen" \
+  "$@" > cloudbuild.log 2>&1 &
 echo "Cloud Build job submitted in the background. Output redirected to cloudbuild.log"
