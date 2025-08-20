b12299f9135d82bb4b18c7e7823a57f487cc9c92
weimeilin79 <weimeilin@gmail.com>
2025-08-20 07:33:50 -0400

    use relative link

--- run_cloudbuild.sh
+++ run_cloudbuild.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 nohup gcloud builds submit --config cloudbuild.yaml \
-  --substitutions=_REGION="$REGION",_REPO_NAME="$REPO_NAME",_SERVICE_NAME="agentverse-dungeon" \
+  --substitutions=_REGION="$REGION",_REPO_NAME="$REPO_NAME",_SERVICE_NAME="agentverse-dungeon",_PROJECT_ID="$PROJECT_ID" \
   "$@" > cloudbuild.log 2>&1 &
 echo "Cloud Build job submitted in the background. Output redirected to cloudbuild.log"
